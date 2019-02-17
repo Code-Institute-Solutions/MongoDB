acd184c84d9be56b4ad58d0462dad87b0e098439
Matt Rudge <mrudge@gmail.com>
2019-02-17 11:38:16 +0000

    Update commands.sh
    Add MongoDB Atlas commands

--- 02-ManipulateDataUsingMongoShell/01-create_and_read_back_data/commands.sh
+++ 02-ManipulateDataUsingMongoShell/01-create_and_read_back_data/commands.sh
@@ -1,7 +1,15 @@
-# Download and install MongoDB
+# Download and install MongoDB client 3.4 for mLab
 wget -q https://git.io/vFb1J -O /tmp/setupmongodb.sh && source /tmp/setupmongodb.sh
 
-# Connect to the Mongo Database
+# Download and install MongoDB client 4.0.6 for Atlas
+wget -q https://git.io/fh7vV -O /tmp/setupmongodb.sh && source /tmp/setupmongodb.sh
+
+# Connect to the Mongo Database on mLab
 # NOTE: REPLACE THE `<dbuser>` & `<dbpassword>` WITH YOUR OWN PERSONAL
 # VALUES (WITHOUT THE ANGULAR BRACKETS)
 mongo ds159812.mlab.com:59812/mytestdb -u <dbuser> -p <dbpassword>
+
+# Connect to the Mongo Database on Atlas
+# NOTE: REPLACE THE `<dbuser>` & `<dbname>` WITH YOUR OWN PERSONAL
+# VALUES (WITHOUT THE ANGULAR BRACKETS)
+mongo "mongodb+srv://myfirstcluster-fsmjm.mongodb.net/<dbname>" --username <dbuser>
